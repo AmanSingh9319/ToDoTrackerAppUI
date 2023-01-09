aec2bd981b69f5109ced99ec0c1f37eb3f273e85
shikhar agrahari <shikharagrahari89@gmail.com>
2023-01-09 15:11:31 +0530

    added task view component

--- ToDoTrackerAppUI/src/app/app.module.ts
+++ ToDoTrackerAppUI/src/app/app.module.ts
@@ -3,10 +3,12 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { ViewTasksComponent } from './components/view-tasks/view-tasks.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    ViewTasksComponent
   ],
   imports: [
     BrowserModule,
