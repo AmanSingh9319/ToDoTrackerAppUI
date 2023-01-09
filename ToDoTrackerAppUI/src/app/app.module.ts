a8d52ca5b9502896fdc6ddc510b3e32679827dca
Nikita Chauhan <nikitachauhan64@gmail.com>
2023-01-09 15:01:34 +0530

    created home, register, login, user profile, archive task component

--- ToDoTrackerAppUI/src/app/app.module.ts
+++ ToDoTrackerAppUI/src/app/app.module.ts
@@ -4,10 +4,24 @@ import { BrowserModule } from '@angular/platform-browser';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { HomeComponent } from './components/home/home.component';
+import { RegisterComponent } from './components/register/register.component';
+import { LoginComponent } from './components/login/login.component';
+import { HelpcenterComponent } from './components/helpcenter/helpcenter.component';
+import { ViewArchiveTaskComponent } from './components/view-archive-task/view-archive-task.component';
+import { UserProfileComponent } from './components/user-profile/user-profile.component';
+import { ArchiveTaskCardComponent } from './components/archive-task-card/archive-task-card.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HomeComponent,
+    RegisterComponent,
+    LoginComponent,
+    HelpcenterComponent,
+    ViewArchiveTaskComponent,
+    UserProfileComponent,
+    ArchiveTaskCardComponent
   ],
   imports: [
     BrowserModule,
---
+++ ToDoTrackerAppUI/src/app/components/view-archive-task/view-archive-task.component.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-view-archive-task',
+  templateUrl: './view-archive-task.component.html',
+  styleUrls: ['./view-archive-task.component.css']
+})
+export class ViewArchiveTaskComponent {
+
+}
