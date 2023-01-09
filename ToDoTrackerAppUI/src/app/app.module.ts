a14a9e1fd776bcccde905911486acb889355546f
Pratik Goud <pratik.loop@gmail.com>
2023-01-09 15:51:03 +0530

    error resolved

--- ToDoTrackerAppUI/src/app/app.module.ts
+++ ToDoTrackerAppUI/src/app/app.module.ts
@@ -1,11 +1,8 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
-
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
-<<<<<<< HEAD
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
-<<<<<<< HEAD
 import { HeaderSidenavComponent } from './components/header-sidenav/header-sidenav.component';
 import { LayoutModule } from '@angular/cdk/layout';
 import { MatToolbarModule } from '@angular/material/toolbar';
@@ -13,7 +10,6 @@ import { MatButtonModule } from '@angular/material/button';
 import { MatSidenavModule } from '@angular/material/sidenav';
 import { MatIconModule } from '@angular/material/icon';
 import { MatListModule } from '@angular/material/list';
-=======
 import { HomeComponent } from './components/home/home.component';
 import { RegisterComponent } from './components/register/register.component';
 import { LoginComponent } from './components/login/login.component';
@@ -21,28 +17,21 @@ import { HelpcenterComponent } from './components/helpcenter/helpcenter.componen
 import { ViewArchiveTaskComponent } from './components/view-archive-task/view-archive-task.component';
 import { UserProfileComponent } from './components/user-profile/user-profile.component';
 import { ArchiveTaskCardComponent } from './components/archive-task-card/archive-task-card.component';
->>>>>>> 5c8c7460c9406d1251d3f4acf8295874f9e82d73
-=======
->>>>>>> 145bf6dceec8ad2a18e54328976f51ccea0119f6
+import { ViewTasksComponent } from './components/view-tasks/view-tasks.component';
 
 @NgModule({
   declarations: [
     AppComponent,
-<<<<<<< HEAD
-<<<<<<< HEAD
-    HeaderSidenavComponent
-=======
+    HeaderSidenavComponent,
     HomeComponent,
     RegisterComponent,
     LoginComponent,
     HelpcenterComponent,
     ViewArchiveTaskComponent,
     UserProfileComponent,
-    ArchiveTaskCardComponent
->>>>>>> 5c8c7460c9406d1251d3f4acf8295874f9e82d73
-=======
+    ArchiveTaskCardComponent,
     ViewTasksComponent
->>>>>>> 145bf6dceec8ad2a18e54328976f51ccea0119f6
+
   ],
   imports: [
     BrowserModule,
