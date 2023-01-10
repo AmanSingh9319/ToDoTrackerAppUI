823c5be1737a3ef60e0e476270c28ef2f3db6aa8
Pratik Goud <pratik.loop@gmail.com>
2023-01-10 16:18:47 +0530

    .

--- ToDoTrackerAppUI/src/app/components/view-filtered-tasks/view-work-tasks/view-work-tasks.component.ts
+++ ToDoTrackerAppUI/src/app/components/view-filtered-tasks/view-work-tasks/view-work-tasks.component.ts
@@ -1,4 +1,7 @@
 import { Component } from '@angular/core';
+import { Task } from 'src/app/model/Task';
+import { User } from 'src/app/model/User';
+import { UserTaskService } from 'src/app/services/user-task.service';
 
 @Component({
   selector: 'app-view-work-tasks',
@@ -6,5 +9,23 @@ import { Component } from '@angular/core';
   styleUrls: ['./view-work-tasks.component.css']
 })
 export class ViewWorkTasksComponent {
+  constructor(private userTaskService:UserTaskService) { }
 
+  user:User={};
+  tasks:Task[] = [];
+
+  getWorkTask(){                                                             //to view personal tasks
+    this.userTaskService.getAllTasksOfUser(this.user).subscribe({
+      next:data => { this.tasks=data.filter((task)=>
+        {
+          return task.taskCategory?.startsWith("work");
+        }) },
+      error() {alert ("error occured while loading the work tasks")},          
+    })
+  }
+
+  ngOnInit(): void {
+    this.getWorkTask();
+   
+  }
 }
